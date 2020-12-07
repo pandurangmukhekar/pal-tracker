9d02c63132f17ac521e411a712fb2ade3d87d334
Ubuntu <ubuntu@ip-172-31-18-41.ap-southeast-1.compute.internal>
2020-12-07 09:40:06 +0000

    first commit

---
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -0,0 +1,14 @@
+package io.pivotal.pal.tracker;
+
+import jdk.jfr.Threshold;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "hello Spring Boot !!!";
+    }
+}
